f0c495c6c3217a541f140ed16de8449e18365191
Julien Girard <julien.girard@arkea.com>
2017-07-21 13:37:01 +0200

    Version 1.1.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openstick',
-    version="1.1.1",
+    version="1.1.2",
     packages=find_packages(),
     author="Julien GIRARD",
     author_email="julien.girard.ju@gmail.com",
